db47e55f0e04e4effe33c6fc51c80578176fb38d
Mariam Kakushadze <mariam.kakushadze3@gmail.com>
2022-09-21 21:11:16 +0400

    made avoided same functions and made code easier

--- script.js
+++ script.js
@@ -5,19 +5,18 @@ const overlay = document.querySelector(".overlay");
 const closeBtn = document.querySelector(".close-modal");
 const showBtn = document.querySelectorAll(".show-modal");
 
-console.log(showBtn);
+const openModal = function () {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
 
 for (let i = 0; i < showBtn.length; i++)
-  showBtn[i].addEventListener("click", function () {
-    modal.classList.remove("hidden");
-    overlay.classList.remove("hidden");
-  });
+  showBtn[i].addEventListener("click", openModal);
 
-closeBtn.addEventListener("click", function () {
-  modal.classList.add("hidden");
-  overlay.classList.add("hidden");
-});
-overlay.addEventListener("click", function () {
-  modal.classList.add("hidden");
-  overlay.classList.add("hidden");
-});
+closeBtn.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
