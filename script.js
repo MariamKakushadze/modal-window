94d9fcfa8bd09493751fbb48e9153143b9cd86ce
Mariam Kakushadze <mariam.kakushadze3@gmail.com>
2022-09-21 21:05:08 +0400

    added click events on btns

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
-'use strict';
+"use strict";
 
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const closeBtn = document.querySelector(".close-modal");
+const showBtn = document.querySelectorAll(".show-modal");
+
+console.log(showBtn);
+
+for (let i = 0; i < showBtn.length; i++)
+  showBtn[i].addEventListener("click", function () {
+    modal.classList.remove("hidden");
+    overlay.classList.remove("hidden");
+  });
+
+closeBtn.addEventListener("click", function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+});
+overlay.addEventListener("click", function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+});
